69e91ac8195ee3263a9b876b31706d47fcf5eda0
jithin k jayaraj <jkjjithinjkj@gmail.com>
2020-06-18 09:40:54 -0700

    first1.2

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('views'))
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+//  res.send('Hello World!');
+res.render('index');
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
